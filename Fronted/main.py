83807ee20122403e3f9d57fd4e9e80d4918a8c39
Luis9092 <luisc522txe@gmail.com>
2024-05-12 22:19:09 -0600

    funcionamiento

--- Fronted/main.py
+++ Fronted/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, session, url_for
 import requests
 import json
+from datetime import datetime
 
 app = Flask(__name__, template_folder="templates")
 app.secret_key = "casae12312"
@@ -24,7 +25,6 @@ def loguear():
         pintar_menu = peticion.json()
         session["menus"] = pintar_menu
 
-        print(pintar_menu)
         return render_template("inicio.html", menu=pintar_menu)
 
     return render_template("login.html", estado="Usuario o contrasenia invalidos!!")
@@ -33,7 +33,7 @@ def loguear():
 @app.route("/Inicio")
 def Inicio():
     doce = session.get("menus")
-    print(doce)
+
     return render_template("inicio.html", menu=doce)
 
 
@@ -57,11 +57,9 @@ def CrearUsuario():
     urlCentro = "http://127.0.0.1:8000/CrearCentro"
     urlObtenerId = "http://127.0.0.1:8000/obteneridI/<nombre>"
     Id = 1
-    
-    
-            
+
     if usertype == "association":
-        print(usertype)
+
         parametrosAs = {
             "id": 0,
             "nombre": institution_name,
@@ -71,17 +69,17 @@ def CrearUsuario():
         }
         x = requests.post(urlCentro, data=json.dumps(parametrosAs))
         parametrosid = {"nombre": institution_name}
-        if x.status_code == 201: 
+        if x.status_code == 201:
             y = requests.get(urlObtenerId, parametrosid)
             data = y.json()
             Id = data["id"]
-            
+
     parametro = {
         "id": 0,
         "username": new_username,
         "correo": str(email),
         "password": str(new_password),
-        "imagenUsuario": "default.png",
+        "imagenUsuario": "default.jpg",
         "fechaCreacion": "",
         "idPerdil": 1,
         "idCentroAyuda": Id,
@@ -90,6 +88,55 @@ def CrearUsuario():
     z = requests.post(urlUsuario, data=json.dumps(parametro))
 
     return render_template("login.html")
+
+
+#  INICIO DE LA PUBLICACION RENDERIZACION
+@app.route("/PublicacionAgregar", methods=["POST"])
+def PublicacionCrear():
+    titulo = request.form["title"]
+    descripcion = request.form["descripcion"]
+    lugar = request.form["lugar"]
+    imagen = request.files["imagen"]
+    urlCrear = "http://127.0.0.1:8000/insertarPublicacion"
+    urlobtnerIdU = "http://127.0.0.1:8000/Obteneneriduser/<correo>"
+    correoUs = request.form["correoUs"]
+    id = 0
+    pCorreous = {"correo": correoUs}
+    x = requests.get(urlobtnerIdU, pCorreous)
+
+    if x.status_code == 200:
+        data = x.json()
+        id = data["id"]
+    print(x)
+
+    tiempo = datetime.now()
+    horaActual = tiempo.strftime("%Y%H%M%S")
+    nm = ""
+    if imagen.filename != "":
+        nm = horaActual + "_" + imagen.filename
+        imagen.save("static/imagenesServer/" + nm)
+
+    parametros = {
+        "id": 0,
+        "titulo": str(titulo),
+        "descripcion": str(descripcion),
+        "lugar": str(lugar),
+        "foto": str(nm),
+        "fechaHora": "",
+        "idUser": id,
+        "estado": 0,
+        "idCentro": 0,
+        "user": "",
+        "correo": "",
+        "imagenUsuario": "",
+    }
+
+    # INSERTANDO NUEVA PUBLICACION
+    y = requests.post(urlCrear, data=json.dumps(parametros))
+
+    doce = session.get("menus")
+
+    return render_template("inicio.html", menu=doce)
 
 
 if __name__ == "__main__":
--- Fronted/static/js/inicio.js
+++ Fronted/static/js/inicio.js
@@ -1,51 +1,134 @@
 var modal = document.getElementById("myModal");
-        var btn = document.getElementById("Publicar");
-        var span = document.getElementsByClassName("close")[0];
-        var form = document.querySelector('form');
-    
-        btn.onclick = function() {
-            modal.style.display = "block";
+var btn = document.getElementById("Publicar");
+var span = document.getElementsByClassName("close")[0];
+var form = document.querySelector('form');
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+    resetForm();
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        resetForm();
+    }
+}
+
+// form.onsubmit = function() {
+//     modal.style.display = "none";
+//     resetForm();
+//     return true;
+// }
+
+function resetForm() {
+    document.querySelector('.textarea-wrapper textarea').value = '';
+    var preview = document.querySelector('.textarea-image');
+    while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+    }
+    document.getElementById('image-upload').value = '';
+}
+
+function previewImage(event) {
+    var input = event.target;
+    var preview = document.querySelector('.textarea-image');
+    while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+    }
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var img = document.createElement('img');
+            img.src = e.target.result;
+            preview.appendChild(img);
         }
-        span.onclick = function() {
-            modal.style.display = "none";
-            resetForm();
-        }
-    
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-                resetForm();
-            }
-        }
-    
-        form.onsubmit = function() {
-            modal.style.display = "none";
-            resetForm();
-            return false;
-        }
-    
-        function resetForm() {
-            document.querySelector('.textarea-wrapper textarea').value = '';
-            var preview = document.querySelector('.textarea-image');
-            while (preview.firstChild) {
-                preview.removeChild(preview.firstChild);
-            }
-            document.getElementById('image-upload').value = '';
-        }
-    
-        function previewImage(event) {
-            var input = event.target;
-            var preview = document.querySelector('.textarea-image');
-            while (preview.firstChild) {
-                preview.removeChild(preview.firstChild);
-            }
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    var img = document.createElement('img');
-                    img.src = e.target.result;
-                    preview.appendChild(img);
-                }
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+const bar = document.querySelector("#bar");
+
+bar.addEventListener("click", (e) => {
+    document.querySelector(".publicaciones-container").classList.toggle("hide");
+    document.querySelector(".bar").classList.toggle("hide");
+})
+
+//MODO DAR AND LIGHT THEME IN THE WEB
+const body = document.querySelector("body");
+let modeToggle = body.querySelector("#switch-mode");
+const sidebar = body.querySelector("#menu_opcion");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swictherTheme = document.querySelector("#switch-mode");
+  const root = document.documentElement;
+
+  if (root.getAttribute("data-theme") === "dark") {
+    swictherTheme.checked = true;
+  }
+
+  function toggleTheme() {
+    const setTheme = this.checked ? "dark" : "light";
+    root.setAttribute("data-theme", setTheme);
+    localStorage.setItem("theme", setTheme);
+  }
+
+  swictherTheme.addEventListener("click", toggleTheme);
+});
+
+// RENDERIZANDO LAS PUBLICACIONES
+window.onload = function () {
+    // Código que se ejecutará cuando la página termine de cargar
+    RenderPublicaciones();
+}
+
+
+function RenderPublicaciones() {
+    axios
+        .get("http://127.0.0.1:8000/VerPublicaciones")
+        .then(function (response) {
+            let tag = "";
+            Object.entries(response.data).forEach(([key, value]) => {
+                // console.log(value.titulo)
+
+                tag += `<div class="publicacion">
+                <div class="usuario">
+                  <img
+                    src="static/imagenesServer/${value.imagenUsuario}"
+                    
+                  />
+                  <div class="usuario-info">
+                    <span id="usuario">${value.user}</span>
+                    <span class="fecha"
+                      >Publicado el ${value.fechaHora}</span
+                    >
+                  </div>
+                </div>
+                <div class="contenido">
+                  <h3>${value.titulo}</h3>
+                  <p>
+                    ${value.descripcion}
+                  </p>
+                  <img
+                    src="static/imagenesServer/${value.foto}"
+                    alt="Imagen de la Publicación"
+                  />
+                  <div class="interacciones">
+                    <button value= "${value.id}"><i class="far fa-thumbs-up"></i> Me gusta</button>
+                    <button value= "${value.id}" ><i class="far fa-comment"></i> Comentar</button>
+                    <button value= "${value.id}" ><i class="fas fa-share"></i> Compartir</button>
+                    <button value= "${value.id}" ><i class="fas fa-paw"></i> Adoptar</button>
+                  </div>
+                </div>
+              </div>`
+
+            })
+            document.querySelector("#PublicacionesP").innerHTML = tag;
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+
+}
